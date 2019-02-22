570be678c02a172a08635e094fb98202f24d8d84
Dunatron <heath.dunlop.hd@gmail.com>
2019-02-22 18:46:17 +1300

    added jwt and cookies for user authentication with server sider rendering

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -9,7 +9,7 @@ const mutations = {
   async signup(parent, args, ctx, info) {
     //lowercase their email
     args.email = args.email.toLowerCase()
-    // hash their password
+    // hash their password. 1 way so if you goy hold of the hashed pass you could not turn it back to what the user actually typed
     const password = await bcrypt.hash(args.password, 10)
     // create the user in the database
     const user = await ctx.db.mutation.createUser(
